7e27c0ffbf516eddbc9caaf87e288906c61f78dd
LeonardD93 <leonard77dam@yahoo.it>
2022-07-29 17:04:46 +0200

    laravel echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,6 +15,21 @@
  // router
  import router from './routes.js';
  window.router = router;
+
+
+import Echo from "laravel-echo"
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'your-pusher-key',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
+
  window.Fire = new Vue();
  
  const app = new Vue({
